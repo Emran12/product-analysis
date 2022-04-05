6fc58116547686f1f2318d1d74512171663db346
Emran12 <emran6452@gmail.com>
2022-04-05 11:07:51 +0600

    NotFound page has been completed

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col justify-center items-center mt-48">
-      <h1 className="text-6xl text-red-700">Oops!</h1>
-      <p>404-PAGE NOT FOUND</p>
-      <p>
+    <div className="flex flex-col justify-center items-center mt-32">
+      <h1 className="text-6xl text-red-700 mb-3 font-serif">Oops!</h1>
+      <p className="font-bold text-2xl font-serif ">404-PAGE NOT FOUND</p>
+      <p className="text-xl font-serif mt-2">
         The page you are looking for might have been removed had its name
         changed or temporarily unavailable.
       </p>
-      <p>
-        <button>GO TO HOMEPAGE</button>
-      </p>
+      <Link to="/">
+        <button className="border-2 rounded-lg bg-rose-500 text-2xl p-4 font-bold text-white mt-3">
+          GO TO HOMEPAGE
+        </button>
+      </Link>
     </div>
   );
 };
